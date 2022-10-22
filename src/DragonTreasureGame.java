f33ad1b17e99ca2cf33fa8dafc1e2fdff052e872
Juyoung <ohjuyoung1006@gmail.com>
2022-10-22 18:25:26 -0500

    4.4 finish

--- src/DragonTreasureGame.java
+++ src/DragonTreasureGame.java
@@ -41,6 +41,8 @@ import processing.core.PImage;
 public class DragonTreasureGame extends PApplet {
 
   private ArrayList<Room> roomList;
+  private File roomInfo;
+  private File mapInfo;
 
   @Override
   public void settings() {
@@ -57,24 +59,14 @@ public class DragonTreasureGame extends PApplet {
     this.focused = true; // window will be active upon running program
     this.textAlign(CENTER); // sets the text alignment to center
     this.textSize(20); // sets the font size for the text
+
+    roomInfo = new File("roominfo.txt");
+    mapInfo = new File("map.txt");
+
     roomList = new ArrayList<Room>();
     Room.setProcessing(this);
     TreasureRoom.setTreasureBackground(this.loadImage("images" + File.separator + "treasure.jpg"));
     PortalRoom.setPortalImage(this.loadImage("images" + File.separator + "portal.png"));
-
-    Room testRoom =
-        new Room(1, "this is a test room", this.loadImage("images" + File.separator + "1.jpg"));
-    roomList.add(testRoom);
-
-    Room startRoom = new StartRoom(2, this.loadImage("images" + File.separator + "1.jpg"));
-    roomList.add(startRoom);
-
-    Room treasureRoom = new TreasureRoom(3);
-    roomList.add(treasureRoom);
-
-    Room portalRoom =
-        new PortalRoom(3, "test portal room", this.loadImage("images" + File.separator + "1.jpg"));
-    roomList.add(portalRoom);
   }
 
   public void draw() {
@@ -83,5 +75,118 @@ public class DragonTreasureGame extends PApplet {
 
   public static void main(String[] args) {
     PApplet.main("DragonTreasureGame");
+  }
+
+  /**
+   * Loads in room info using the file stored in roomInfo
+   * 
+   * @author Michelle
+   */
+  private void loadRoomInfo() {
+    System.out.println("Loading rooms...");
+    Scanner fileReader = null;
+    try {
+
+      // scanner to read from file
+      fileReader = new Scanner(roomInfo);
+
+      // read line by line until none left
+      while (fileReader.hasNext()) {
+        String nextLine = fileReader.nextLine();
+
+        // parse info and create new room
+        String[] parts = nextLine.split(" \\| ");
+        int ID = Integer.parseInt(parts[1].trim()); // get the room id
+        String imageName = null;
+        String description = null;
+        PImage image = null;
+        Room newRoom = null;
+
+        if (parts.length >= 3) {
+          imageName = parts[2].trim();
+          image = this.loadImage("images" + File.separator + imageName);
+
+        }
+
+        if (parts.length == 4) {
+          description = parts[3].trim(); // get the room description
+        }
+
+        switch (parts[0].trim()) {
+          case "S":
+            newRoom = new StartRoom(ID, image);
+            break;
+          case "R":
+            newRoom = new Room(ID, description, image);
+            break;
+          case "P":
+            newRoom = new PortalRoom(ID, description, image);
+            break;
+          case "T":
+            newRoom = new TreasureRoom(ID);
+            break;
+          default:
+            break;
+        }
+
+        if (newRoom != null) {
+          roomList.add(newRoom);
+        }
+      }
+    } catch (IOException e) { // handle checked exception
+      e.printStackTrace();
+    } finally {
+      if (fileReader != null)
+        fileReader.close(); // close scanner regardless of what happened for security reasons :)
+    }
+  }
+
+  /**
+   * Loads in room connections using the file stored in mapInfo
+   * 
+   * @author Michelle
+   */
+  private void loadMap() {
+    System.out.println("Loading map...");
+    Scanner fileReader = null;
+    try {
+      // scanner to read from file
+      fileReader = new Scanner(mapInfo);
+
+      // read line by line until none left
+      while (fileReader.hasNext()) {
+
+        // parse info
+        String nextLine = fileReader.nextLine();
+        String parts[] = nextLine.split(" ");
+        int id = Integer.parseInt(parts[0]);
+
+        Room toEdit = getRoomByID(id); // get the room we need to update info for adjacent rooms
+
+        // add all the rooms to the adj room list of toEdit
+        for (int i = 1; i < parts.length; i++) {
+          Room toAdjAdd = getRoomByID(Integer.parseInt(parts[i]));
+          toEdit.addToAdjacentRooms(toAdjAdd);
+        }
+      }
+    } catch (IOException e) { // handle checked exception
+      e.printStackTrace();
+    } finally { // close scanner regardless of what happened for security reasons :)
+      if (fileReader != null)
+        fileReader.close();
+    }
+  }
+
+  /**
+   * Get the room objected associated with the given ID.
+   * 
+   * @param id the ID of the room to retrieve
+   * @return the Room that corresponds to that id
+   * @author Michelle
+   */
+  private Room getRoomByID(int id) {
+    int indexToEdit = roomList.indexOf(new Room(id, "dummy", null));
+    Room toEdit = roomList.get(indexToEdit);
+    return toEdit;
   }
 }
